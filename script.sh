9a5b10345b7e8c3a1234dedb5139c9c5c7ad5705
Vladyslav Samoilenko <vladyslav.samoilenko@proton.me>
2023-01-13 16:49:01 +0200

    fix

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-IFS=' ' read -r -a array <<< `poetry version | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g"`; return "${array[$1]}"
+IFS=' ' read -r -a array <<< `poetry version | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g"`; echo "${array[$1]}"
